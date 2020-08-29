e9b5b82982c6d6aa0d8ae0840eb108611ad92d34
Raúl Caro Pastorino <dev@fryntiz.es>
2020-08-29 19:33:00 +0200

    Creo base para modelo de keyboard independiente

---
+++ Models/KeyboardLogger.py
@@ -0,0 +1,61 @@
+#!/usr/bin/python3
+# -*- encoding: utf-8 -*-
+
+# @author     Raúl Caro Pastorino
+# @email      dev@fryntiz.es
+# @web        https://fryntiz.es
+# @gitlab     https://gitlab.com/fryntiz
+# @github     https://github.com/fryntiz
+# @twitter    https://twitter.com/fryntiz
+# @telegram   https://t.me/fryntiz
+
+# Create Date: 2020
+# Project Name: Python Keylogger
+# Description:
+
+#
+# Dependencies: keyboard
+#
+# Revision 0.01 - File Created
+# Additional Comments:
+
+# @copyright  Copyright © 2020 Raúl Caro Pastorino
+# @license    https://wwww.gnu.org/licenses/gpl.txt
+
+# Copyright (C) 2020  Raúl Caro Pastorino
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>
+
+# Guía de estilos aplicada: PEP8
+
+#######################################
+# #           Descripción           # #
+#######################################
+# Description:
+
+#######################################
+# #       Importar Librerías        # #
+#######################################
+
+#######################################
+# #             Variables           # #
+#######################################
+
+#######################################
+# #              Clase              # #
+#######################################
+
+class KeyboardLogger:
+    # Nombre de la tabla para almacenar datos
+    tablename = 'keyboard'
